c90dd79211b1e123b33d8fa934f97393423536eb
devsecop <test@DESKTOP-HRJUNEU.localdomain>
2026-05-03 08:19:51 +0800

    Add vulnerable dependencies for SCA test 3

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="security-lab",
+    version="0.1",
+    install_requires=[
+        "Flask==0.12.2",
+        "requests==2.19.1",
+        "PyYAML==3.12",
+    ],
+)
