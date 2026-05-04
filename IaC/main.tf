1f4cb54f13da7c8c74c486f227f92f132bbcb510
devsecop <test@DESKTOP-HRJUNEU.localdomain>
2026-05-04 10:47:43 +0800

    comment out s3 module

--- IaC/main.tf
+++ IaC/main.tf
@@ -3,8 +3,9 @@ module "vpc_infrastructure" {
   # You can pass variables from the root to the child here
   #vpc_name = "cyber-lab-vpc"
 }
-
+/*
 module "s3_storage" {
   source = "./S3"
 }
+*/
 # Triggering security scan
