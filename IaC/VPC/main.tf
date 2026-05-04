c8dce5427dc281758ba5469cd392c833b60e81dc
devsecop <test@DESKTOP-HRJUNEU.localdomain>
2026-05-04 10:12:40 +0800

    Fix version issue

--- IaC/VPC/main.tf
+++ IaC/VPC/main.tf
@@ -1,6 +1,7 @@
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
+  #source  = "terraform-aws-modules/vpc/aws"
+  #version = "5.0.0"
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=26c38a66f12e7c6c93b6a2ba127ad68981a48671"  # commit hash of version 5.0.0
 
   azs = var.availability_zones
   private_subnets = var.private_subnets
